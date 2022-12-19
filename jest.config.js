114986c40eba6bb2221f9edc8460dc04ba9cbfd2
Dave Adams <dave.adams@boughtbymany.com>
2022-12-19 10:10:17 +0000

    feat: get tests running and passing

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-	setupFiles: [
-		'<rootDir>/tests/js/setupTests.js',
-	],
-	testMatch: [
-		'**/tests/js/**/*.test.js',
-	],
+  setupFiles: ["<rootDir>/tests/js/setupTests.js"],
+  testMatch: ["**/tests/js/**/*.test.js"],
+  testEnvironment: "node",
 };
